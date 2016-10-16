88883e81578b8f063dd6e14c853613a7c793cdca
15Cyndaquil <john.tyler.gafford@gmail.com>
2016-10-15 22:49:18 -0400

    No more submit
    Set up Listener so Rectangle.java could calculate while user was
    inputing decreasing need for additional user input.

--- Rectangle.java
+++ Rectangle.java
@@ -1,4 +1,5 @@
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.HPos;
@@ -12,11 +13,11 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
 
+
 public class Rectangle extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) {
-		
 		//Created grid with 2 columns and 6 rows 
 		//Columns are 300 pixels long
 		//Rows are 50 pixels long
@@ -29,13 +30,15 @@ public class Rectangle extends Application {
 			RowConstraints row = new RowConstraints(50);
 			grid.getRowConstraints().add(row);
 		}
-
+		
 		//Creating text in the middle of column one
 		//which occupies both rows
-		Label text = new Label("Input the height and width of your rectangle");
+		Label text = new Label("Input the height and w");
 		grid.add(text, 0, 0);
-		GridPane.setHalignment(text, HPos.CENTER);
-		GridPane.setColumnSpan(text, 2);
+		GridPane.setHalignment(text, HPos.RIGHT);
+		Label text1 = new Label("idth of your rectangle");
+		grid.add(text1, 1, 0);
+		GridPane.setHalignment(text1, HPos.LEFT);
 		
 		//Creating labels above text inputs
 		Label heightl = new Label("Height");
@@ -48,22 +51,14 @@ public class Rectangle extends Application {
 		
 		//Creating text user text inputs
 		TextField height = new TextField();
-		height.setPromptText("Enter the height");
+		height.setText("Enter the height");
 		grid.add(height, 0, 2);
 		
 		TextField width = new TextField();
-		width.setPromptText("Enter the width");
+		width.setText("Enter the width");
 		grid.add(width, 1, 2);
 		
-		//Creating buttons for submiting user inputs
-		//and for exiting the program
-		Button submit = new Button("Submit");
-		//Alows submit button to be activated by enter
-		submit.setDefaultButton(true);
-		grid.add(submit, 0, 3);
-		GridPane.setHalignment(submit, HPos.CENTER);
-		GridPane.setValignment(submit, VPos.CENTER);
-		
+		//Creating button for exiting the program		
 		Button cancel = new Button("Cancel");
 		GridPane.setHalignment(cancel, HPos.CENTER);
 		GridPane.setValignment(cancel, VPos.CENTER);
@@ -77,44 +72,30 @@ public class Rectangle extends Application {
 		Label output2 =new Label();
 		grid.add(output2, 1, 5);
 		GridPane.setHalignment(output2, HPos.CENTER);
-				
-		//Setting up event for when submit button is pressed
-		submit.setOnAction(new EventHandler<ActionEvent>(){
 		
-			@Override
-			public void handle(ActionEvent submita){
-				String width1;
-				String height1;
-				String areaf,perimiterf;
-				double widthf;
-				double heightf;
-				double area;
-				double perimiter;
-				//Using try to weed out any non number inputs
-				try
-				{
-					width1 = width.getText();
-					height1 = height.getText();
-					widthf = Double.parseDouble(width1);
-					heightf = Double.parseDouble(height1);
-					perimiter = 2*widthf + 2*heightf;
-					area = widthf*heightf;
-					areaf = String.valueOf(area);
-					perimiterf = String.valueOf(perimiter);
-					output1.setText("The Perimiter of your rectangle is " + perimiterf);
-					output2.setText("The Area of your rectangle is " + areaf);
-				}
-				//Reseting outputs and saying that you did not input numbers 
-				catch (NumberFormatException f)
-				{
-					output2.setText("");
-					output1.setText("You did not input numbers");
-				}
-			
+		//Allowing user input to start data processing
+		ChangeListener<String> calculate1 = (observable1, oldValue1, newValue1) -> {
+			//Using try to weed out any non number inputs
+			try{
+				double widthf1 = Double.parseDouble(width.getText());
+				double heightf1 = Double.parseDouble(height.getText());
+				double perimiterf1 = 2*widthf1+2*heightf1;
+				double areaf1 = widthf1*heightf1;
+				output1.setText("The Perimiter of your rectangle is " + perimiterf1);
+				output2.setText("The Area of your rectangle is " + areaf1);
 			}
-		});
+			//Reseting outputs and saying input numbers only
+			//NumberFormatException occurs when TextField width or height
+			//Don't have a double value inside of them
+			catch(NumberFormatException f){
+				output2.setText("");
+				output1.setText("Please input numbers only");
+			}
+		};
+		width.textProperty().addListener(calculate1);
+		height.textProperty().addListener(calculate1);
 		
-		//Setting up cancel button to close program		
+		//Setting up cancel button to close program	
 		cancel.setOnAction(new EventHandler<ActionEvent>(){
 		
 			@Override
